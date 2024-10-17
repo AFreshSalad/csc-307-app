db0bdf02282ce2c32d911ea24c039c4c4022df71
Brian <mhoanguyen68@gmail.com>
2024-10-17 15:01:10 -0700

    implemented DELETE on backend.

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -6,18 +6,12 @@ import Form from "./Form";
 function MyApp() {
   const [characters, setCharacters] = useState([]);
 
-  function removeOneCharacter(index) {
-    const updated = characters.filter((charcter, i) => {
-      return i !== index;
-    });
-    setCharacters(updated);
-  }
-
+  //HTTP Calls
   function fetchUsers() {
     const promise = fetch("http://localhost:8000/users");
     return promise;
   }
-
+  
   function postUser(person) {
     const promise = fetch("http://localhost:8000/users", {
       method: "POST",
@@ -26,8 +20,46 @@ function MyApp() {
       },
       body: JSON.stringify(person)
     });
-  
+    
     return promise;
+  }
+  
+  function deleteUser(id) {
+    const promise = fetch(`http://localhost:8000/users/${id}`, {
+      method: "DELETE"
+    });
+
+    return promise;
+  }
+
+  useEffect(() => {
+    fetchUsers()
+      .then((res) => res.json())
+      .then((json) => setCharacters(json["users_list"]))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
+  //Frontend functions
+  function removeOneCharacter(index) {
+    const userToDelete = characters[index];
+    deleteUser(userToDelete.id)
+      .then((res) => {
+        if (res.status === 204) {
+          const updated = characters.filter((character, i) => {
+            return i !== index;
+          });
+          setCharacters(updated);
+        } else if (res.status === 404) {
+          throw new Error("User not found.");
+        } else {
+          throw new Error("Failed to delete user.");
+        }
+      }) 
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   function updateList(person) {
@@ -46,15 +78,6 @@ function MyApp() {
         console.log(error);
       });
   }
-
-  useEffect(() => {
-    fetchUsers()
-      .then((res) => res.json())
-      .then((json) => setCharacters(json["users_list"]))
-      .catch((error) => {
-        console.log(error);
-      });
-  }, []);
 
   return (
     <div className="container">
