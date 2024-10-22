21e668b692478e6cae4386775cfe12f2ed21bbcc
Brian <mhoanguyen68@gmail.com>
2024-10-22 01:43:43 -0700

    some progress on starter code

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -1,56 +1,18 @@
 import express from "express";
 import cors from "cors";
+import { addUser, getUsers, findUserById, findUserByName, findUserByJob } from "user-service";
 
 const app = express();
 const port = 8000;
-const users = {
-  users_list: [
-    {
-      id: "xyz789",
-      name: "Charlie",
-      job: "Janitor"
-    },
-    {
-      id: "abc123",
-      name: "Mac",
-      job: "Bouncer"
-    },
-    {
-      id: "ppp222",
-      name: "Mac",
-      job: "Professor"
-    },
-    {
-      id: "yat999",
-      name: "Dee",
-      job: "Aspring actress"
-    },
-    {
-      id: "zap555",
-      name: "Dennis",
-      job: "Bartender"
-    }
-  ]
-};
-  
+
+//delete this functionality
 const findUserByNameAndJob = (name, job) => {
   return users["users_list"].filter(user => {
     return (!name || user.name === name) && (!job || user.job === job);
   });
 };
 
-const findUserById = (id) => {
-  users["users_list"].find(
-    (user) => user["id"] === id
-  );
-};
-
-const addUser = (user) => {
-  user.id = Math.random().toString();
-  users["users_list"].push(user);
-  return user;
-};
-
+//add this functionality later in user-service
 const deleteUser = (id) => {
   const index = users["users_list"].findIndex(user => user.id === id);
   if (index !== -1) {
@@ -66,8 +28,14 @@ app.use(express.json());
 
 app.post("/users", (req, res) => {
   const userToAdd = req.body;
-  addUser(userToAdd);
-  res.status(201).send(userToAdd);
+  addUser(userToAdd)
+    .then((userAdded) => {
+      res.status(201).send(userAdded);
+    })
+    .catch((error) => {
+      //Failed to add user.
+      res.status(500).send(error);
+    })
 });
 
 app.delete("/users/:id", (req, res) => {
@@ -80,6 +48,7 @@ app.delete("/users/:id", (req, res) => {
   }
 });
 
+//need to adjust this later with name and job requests
 app.get("/users", (req, res) => {
   const name = req.query.name; //or req.query.['name']
   const job = req.query.job;
@@ -95,12 +64,18 @@ app.get("/users", (req, res) => {
 
 app.get("/users/:id", (req, res) => {
   const id = req.params["id"]; //or req.params.id
-  let result = findUserById(id);
-  if (result === undefined) {
-    res.status(404).send("Resource not found.");
-  } else {
-    res.send(result);
-  }
+  findUserById(id)
+    .then((result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(404).send("Resource not found.");
+      }
+    })
+    .catch((error) => {
+      //Failed to fetch user.
+      res.status(500).send(error);
+    });
 });
 
 app.get("/", (req, res) => {
